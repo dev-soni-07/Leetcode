d21db8f032f25aba2ef99df816218b91dcb6b9dc
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2024-03-03 16:54:25 +0530

    Time: 21 ms (72.29%) | Memory: 11.7 MB (78.31%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxPower(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        max_count = 0
+        previous = None
+        for c in s:
+            if c == previous:
+                count += 1
+            else:
+                previous = c
+                count = 1
+            max_count = max(max_count, count)
+        return max_count
