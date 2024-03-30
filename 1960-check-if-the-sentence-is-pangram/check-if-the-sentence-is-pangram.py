1b15fc7c84b04b7fb83c2d9e9d8f10f51834b440
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2024-03-30 23:57:52 +0530

    Time: 24 ms (5.32%) | Memory: 11.7 MB (64.99%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -4,13 +4,8 @@ class Solution(object):
         :type sentence: str
         :rtype: bool
         """
-        alphabets = set(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
-
-        for word in sentence:
-            if word in alphabets:
-                alphabets.remove(word)
-
-        if not alphabets:
-            return True
-        else:
-            return False
+        unique_letters = set()
+        for char in sentence:
+            unique_letters.add(char)
+        
+        return len(unique_letters) == 26
