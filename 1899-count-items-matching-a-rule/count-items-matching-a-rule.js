09cd122de8946922a4ca489999cb2ac3a85f2a6d
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2024-06-04 18:25:49 +0530

    Time: 64 ms (67.81%) | Memory: 53 MB (55.79%) - LeetSync

---
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function(items, ruleKey, ruleValue) {
+    let rulekeyno;
+    if(ruleKey === "type"){
+        rulekeyno = 0;
+    } else if(ruleKey === "color"){
+        rulekeyno = 1;
+    } else if(ruleKey === "name"){
+        rulekeyno = 2;
+    }
+
+    let matched = 0;
+    for(let i=0; i<items.length; i++){
+        if(items[i][rulekeyno] === ruleValue){
+            matched += 1;
+        }
+    }
+
+    return matched;
+};
