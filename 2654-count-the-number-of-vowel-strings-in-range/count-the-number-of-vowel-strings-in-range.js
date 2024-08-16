2b07fbcaca5da4c8d658bcbe1c5bd4ede40a7b06
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2024-08-16 12:25:07 +0530

    Time: 80 ms (57.48%) | Memory: 54.5 MB (27.57%) - LeetSync

---
+++ 2654-count-the-number-of-vowel-strings-in-range/count-the-number-of-vowel-strings-in-range.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} words
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+var vowelStrings = function(words, left, right) {
+    let vowels = "aeiou", noOfVowelStrings = 0;
+
+    for (let i = left; i <= right; i++) {
+        if (vowels.includes(words[i][0]) && vowels.includes(words[i][words[i].length - 1])) {
+            noOfVowelStrings++;
+        }
+    }
+
+    return noOfVowelStrings;
+};
