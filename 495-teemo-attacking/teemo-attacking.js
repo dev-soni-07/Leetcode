2bafa9f9886ccac0b4310eda65031b074f5f9058
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2024-10-07 22:07:48 +0530

    Time: 66 ms (23.25%) | Memory: 53.6 MB (10.09%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+    if (timeSeries.length === 0 || duration === 0) return 0;
+
+    let totalDuration = 0;
+
+    for (let i = 0; i < timeSeries.length - 1; i++) {
+        totalDuration += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+    }
+
+    totalDuration += duration;
+
+    return totalDuration;
+};
