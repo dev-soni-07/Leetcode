c41773ae341899be9ebc6bfefc64ec4f6f04ce2e
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2024-11-09 09:18:34 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (24.07%) - LeetSync

---
+++ 1764-maximum-repeating-substring/maximum-repeating-substring.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} sequence
+ * @param {string} word
+ * @return {number}
+ */
+var maxRepeating = function(sequence, word) {
+     let k = 0;
+    let repeatedWord = word;
+
+    while (sequence.includes(repeatedWord)) {
+        k++;
+        repeatedWord += word;
+    }
+
+    return k;
+};
