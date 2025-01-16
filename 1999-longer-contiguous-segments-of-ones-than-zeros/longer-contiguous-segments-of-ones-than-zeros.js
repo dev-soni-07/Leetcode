88c13f02d2cb10755a777cf4f78f6e4564461614
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-01-16 08:23:08 +0530

    Time: 1 ms (64.62%) | Memory: 48.6 MB (93.85%) - LeetSync

---
+++ 1999-longer-contiguous-segments-of-ones-than-zeros/longer-contiguous-segments-of-ones-than-zeros.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkZeroOnes = function(s) {
+    let maxOnes = 0, maxZeros = 0;
+    let currentOnes = 0, currentZeros = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '1') {
+            currentOnes++;
+            currentZeros = 0;
+        } else {
+            currentZeros++;
+            currentOnes = 0;
+        }
+        maxOnes = Math.max(maxOnes, currentOnes);
+        maxZeros = Math.max(maxZeros, currentZeros);
+    }
+
+    return maxOnes > maxZeros;
+};
