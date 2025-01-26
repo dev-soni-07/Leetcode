149aed286db947ec1b7219e96d8ff10ee8043c8e
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-01-26 12:38:58 +0530

    Time: 0 ms (100.00%) | Memory: 49.1 MB (44.74%) - LeetSync

---
+++ 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
+    if (s === s.split('').reverse().join('')) {
+        return 1;
+    }
+    return 2;
+};
