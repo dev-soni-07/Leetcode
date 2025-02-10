5875779e7f2ec5cc084f74e9bac7c9a441bd1eda
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-02-10 08:44:35 +0530

    Time: 0 ms (100%), Space: 49.6 MB (42.42%) - LeetHub

---
+++ 2580-circular-sentence/2580-circular-sentence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var isCircularSentence = function(sentence) {
+    const words = sentence.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+        const nextWord = words[(i + 1) % words.length];
+        if (currentWord[currentWord.length - 1] !== nextWord[0]) {
+            return false;
+        }
+    }
+
+    return true;
+};
