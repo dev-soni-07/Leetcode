7cf32c5f396238ffe6fcc8ac2e645a2b5791e0e3
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-04-06 13:07:32 +0530

    Time: 3 ms (45.45%) | Memory: 56.1 MB (24.24%) - LeetSync

--- 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.js
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.js
@@ -1,3 +1,35 @@
-const regexp = /(20)|(31)|(4[20])|(5[31])|(6[420])|(7[531])|(8[6420])|(9[7531])/;
-const getSmallestString = (s, re = regexp.exec(s)) => 
-    re ? s.replace(regexp, re[0][1] + re[0][0]) : s;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function getSmallestString(s) {
+    const input = s.split("")
+    for (let i = 0; i < input.length - 1; ++i) {
+        const charA = input[i]
+        const charB = input[i + 1]
+        if (
+            isSameParity(charA, charB) &&
+            charB.charCodeAt(0) < charA.charCodeAt(0)
+        ) {
+            input[i] = charB
+            input[i + 1] = charA
+            break
+        }
+    }
+    
+    return input.join("")
+};
+
+function isSameParity(charA, charB) {
+    const numberA = Number(charA)
+    const numberB = Number(charB)
+
+    return (
+        isEven(numberA) && isEven(numberB) ||
+        !isEven(numberA) && !isEven(numberB)
+    )
+}
+
+function isEven(number) {
+    return number % 2 === 0
+}
