aebd0ebb5d0ad96b932f8520bd704ec6da6f6cb0
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-08-29 13:12:22 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (54.08%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.js
+++ 3952-trionic-array-i/trionic-array-i.js
@@ -8,21 +8,17 @@ var isTrionic = function(nums) {
 
     let i = 0;
 
-    // Step 1: strictly increasing
     while (i + 1 < n && nums[i] < nums[i + 1]) i++;
-    if (i === 0) return false; // must increase at least once
+    if (i === 0) return false;
 
     let j = i;
 
-    // Step 2: strictly decreasing
     while (j + 1 < n && nums[j] > nums[j + 1]) j++;
-    if (j === i) return false; // must decrease at least once
+    if (j === i) return false;
 
     let k = j;
 
-    // Step 3: strictly increasing
     while (k + 1 < n && nums[k] < nums[k + 1]) k++;
 
-    // Must end exactly at n-1 and have at least 1 step in final increase
     return k === n - 1 && k > j;
 };
