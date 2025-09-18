022e92e9d64e4baa65565935ea30c057ada8c930
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-09-18 13:47:37 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (21.38%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    function findBound(isFirst) {
+        let left = 0, right = nums.length - 1, bound = -1;
+
+        while (left <= right) {
+            let mid = Math.floor((left + right) / 2);
+
+            if (nums[mid] === target) {
+                bound = mid;
+                if (isFirst) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return bound;
+    }
+
+    let first = findBound(true);
+    let last = findBound(false);
+    return [first, last];
+};
