2cd0afeed3f550dd34fec0978035ee36c4af9cf7
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-09-25 13:39:26 +0530

    Time: 2 ms (77.96%) | Memory: 60 MB (32.13%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    nums.sort((a, b) => a - b);
+    let res = [];
+    let used = new Array(nums.length).fill(false);
+
+    function backtrack(path) {
+        if (path.length === nums.length) {
+            res.push([...path]);
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+            used[i] = true;
+            path.push(nums[i]);
+            backtrack(path);
+            path.pop();
+            used[i] = false;
+        }
+    }
+
+    backtrack([]);
+    return res;
+};
