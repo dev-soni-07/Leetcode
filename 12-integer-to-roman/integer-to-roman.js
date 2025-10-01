5d61ded599e420ac2dc2187540d389cc9bc4b6af
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2025-10-01 14:37:04 +0530

    Time: 5 ms (63.07%) | Memory: 60.2 MB (91.87%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+    const vals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const syms = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    
+    let res = "";
+    
+    for (let i = 0; i < vals.length; i++) {
+        while (num >= vals[i]) {
+            num -= vals[i];
+            res += syms[i];
+        }
+    }
+    
+    return res;
+};
