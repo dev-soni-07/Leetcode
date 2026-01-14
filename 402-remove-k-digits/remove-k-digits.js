42ae168baf7a7f31ed83034b0bfb17fc1868429e
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-01-14 12:38:13 +0530

    Time: 13 ms (36.64%) | Memory: 60.7 MB (57.63%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    let stack = [];
+
+    for (let digit of num) {
+        while (stack.length > 0 && k > 0 && stack[stack.length - 1] > digit) {
+            stack.pop();
+            k--;
+        }
+        stack.push(digit);
+    }
+
+    while (k > 0) {
+        stack.pop();
+        k--;
+    }
+
+    let result = stack.join("").replace(/^0+/, "");
+
+    return result === "" ? "0" : result;
+};
