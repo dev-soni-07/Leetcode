a8c1bc0ab7be2f8836518b116e60c686af4ebc7c
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-03-13 11:14:36 +0530

    Time: 2 ms (53.66%) | Memory: 60.4 MB (56.10%) - LeetSync

---
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var tree2str = function(root) {
+    if (!root) return "";
+
+    let result = "" + root.val;
+
+    if (root.left) {
+        result += "(" + tree2str(root.left) + ")";
+    }
+
+    if (root.right) {
+        if (!root.left) {
+            result += "()";
+        }
+        result += "(" + tree2str(root.right) + ")";
+    }
+
+    return result;
+};
