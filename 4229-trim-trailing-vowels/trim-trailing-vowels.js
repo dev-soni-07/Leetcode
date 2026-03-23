c720ca1f30150838a68bd45e7d819ecb91d91b3d
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-03-23 10:59:20 +0530

    Time: 3 ms (6.55%) | Memory: 57.3 MB (19.10%) - LeetSync

---
+++ 4229-trim-trailing-vowels/trim-trailing-vowels.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var trimTrailingVowels = function(s) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    
+    let i = s.length - 1;
+    
+    while (i >= 0 && vowels.has(s[i])) {
+        i--;
+    }
+    
+    return s.substring(0, i + 1);
+};
