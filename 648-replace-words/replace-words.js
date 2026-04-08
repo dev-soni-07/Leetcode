bd004ab0c02d391b5950a13a2486b7fd8de94d44
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-04-08 11:01:28 +0530

    Time: 1037 ms (6.54%) | Memory: 68.6 MB (85.05%) - LeetSync

---
+++ 648-replace-words/replace-words.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function(dictionary, sentence) {
+    const set = new Set(dictionary);
+
+    return sentence.split(" ").map(word => {
+        for (let i = 1; i <= word.length; i++) {
+            let prefix = word.slice(0, i);
+            if (set.has(prefix)) return prefix;
+        }
+        return word;
+    }).join(" ");
+};
