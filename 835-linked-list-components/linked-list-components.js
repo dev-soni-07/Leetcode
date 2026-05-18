73f25ff555de4d3e9b27df886bb76bfd213c4f4f
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-05-18 11:41:54 +0530

    Time: 3 ms (72.97%) | Memory: 60.6 MB (28.38%) - LeetSync

---
+++ 835-linked-list-components/linked-list-components.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numComponents = function(head, nums) {
+    const set = new Set(nums);
+    let count = 0;
+
+    while (head) {
+        if (
+            set.has(head.val) &&
+            (!head.next || !set.has(head.next.val))
+        ) {
+            count++;
+        }
+
+        head = head.next;
+    }
+
+    return count;
+};
