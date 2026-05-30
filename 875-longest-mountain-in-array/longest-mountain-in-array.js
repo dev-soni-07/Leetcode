201ff5893287704a87f905595af3751e3dc547f4
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-05-30 13:04:50 +0530

    Time: 1 ms (96.62%) | Memory: 57.4 MB (51.35%) - LeetSync

---
+++ 875-longest-mountain-in-array/longest-mountain-in-array.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var longestMountain = function(arr) {
+    let up = 0;
+    let down = 0;
+    let ans = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+
+        // reset when:
+        // 1. new mountain starts after descending
+        // 2. equal elements break mountain
+        if ((down > 0 && arr[i] > arr[i - 1]) ||
+            arr[i] === arr[i - 1]) {
+            up = 0;
+            down = 0;
+        }
+
+        if (arr[i] > arr[i - 1]) {
+            up++;
+        } else if (arr[i] < arr[i - 1]) {
+            down++;
+        }
+
+        if (up > 0 && down > 0) {
+            ans = Math.max(ans, up + down + 1);
+        }
+    }
+
+    return ans;
+};
