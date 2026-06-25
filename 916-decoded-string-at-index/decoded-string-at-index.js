377bf2e99622f255a18f980bf711cf2a7c162f80
Dev Soni <112820453+dev-soni-07@users.noreply.github.com>
2026-06-25 17:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (33.33%) - LeetSync

---
+++ 916-decoded-string-at-index/decoded-string-at-index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var decodeAtIndex = function(s, k) {
+    let size = 0n;
+    let K = BigInt(k);
+
+    // Calculate decoded length
+    for (const ch of s) {
+        if (ch >= '0' && ch <= '9') {
+            size *= BigInt(ch);
+        } else {
+            size++;
+        }
+    }
+
+    // Work backwards
+    for (let i = s.length - 1; i >= 0; i--) {
+        const ch = s[i];
+
+        K %= size;
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (K === 0n) return ch;
+            size--;
+        } else {
+            size /= BigInt(ch);
+        }
+    }
+};
